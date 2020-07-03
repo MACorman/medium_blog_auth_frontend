ea6ae503653848f73f7115de3b7d97599c225a1a
MACorman <madelinecorman@gmail.com>
2020-07-03 15:58:28 -0400

    built simple frontend of signup form, login form, and profile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LoginSignUp from './LoginSignUp';
+import Profile from './Profile'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    currentUser: {}
+  }
+
+  createUser = (user) => {
+    fetch('http://localhost:3000/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ user })
+    })
+    .then(resp => resp.json())
+    .then(user => this.getProfile(user))
+
+  }
+
+  login = (user) => {
+    fetch('http://localhost:3000/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }, 
+      body: JSON.stringify({ user })
+    })
+    .then(resp => resp.json())
+    .then(user => {
+      this.getProfile(user)
+    })
+  }
+
+  getProfile = (user) => {
+    fetch('http://localhost:3000/api/v1/profile', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${user.jwt}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(user => this.setState({currentUser: user.user}))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.currentUser.id ? <Profile currentUser={this.state.currentUser} /> : <LoginSignUp login={this.login} createUser={this.createUser}/>}
+      </div>
+    );
+  }
 }
 
 export default App;
